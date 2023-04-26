0849c7c12217085125a2d1791792cde1e2c9cf7b
Kiriela <aki.koulu@gmail.com>
2023-04-26 22:24:23 +0300

    Changed structure of frames

--- calc.py
+++ calc.py
@@ -4,30 +4,36 @@ import communicator
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("dark-blue")
 
+BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", ",", "="]
+
 class CalculatorWindow(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, mainwindow, parent):
         super().__init__(parent)
+        self.__mw = mainwindow
 
-        self.__registrationframe = ctk.CTkFrame(master=parent)
-        self.__registrationframe.grid(row=0, column=0)
+        self.__calculatorframe = ctk.CTkFrame(master=parent)
+        self.__calculatorframe.grid(row=0, column=0)
 
-        username_entry = ctk.CTkEntry(master=self.__registrationframe, placeholder_text="Wow")
-        username_entry.pack(padx=10, pady=8)
-        password_entry = ctk.CTkEntry(master=self.__registrationframe, placeholder_text="Wow", show="*")
-        password_entry.pack(padx=10, pady=8)
+        buttoncount = 0
+        for x in range(4):
+            for y in range(4):
+                username_entry = ctk.CTkButton(master=self.__calculatorframe, text=BUTTONS[buttoncount])
+                username_entry.grid(row=x, column=y)
+                buttoncount += 1
 
         self.hide()
     
     def hide(self):
-        self.__registrationframe.grid_forget()
+        self.__calculatorframe.grid_forget()
 
     def show(self):
-        self.__registrationframe.grid(row=0, column=0)
+        self.__calculatorframe.grid(row=0, column=0)
 
 
 class LoginWindow(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, mainwindow, parent):
         super().__init__(parent)
+        self.__mw = mainwindow
         self.__loginframe = ctk.CTkFrame(master=parent)
         self.__loginframe.grid(row=0, column=0, sticky=ctk.E+ctk.W+ctk.S+ctk.N)
 
@@ -39,21 +45,30 @@ class LoginWindow(ctk.CTkFrame):
         self.__password_entry = ctk.CTkEntry(master=self.__loginframe, placeholder_text="Password", show="*")
         self.__password_entry.pack(padx=10, pady=8)
 
-        self.hide()
+        credential_button = ctk.CTkButton(master=self.__loginframe, text="Login", command=self.log)
+        credential_button.pack(padx=10, pady=8)
+
+        credential_label = ctk.CTkButton(master=self.__loginframe, text="Create an account?", command=self.move_to_reg)
+        credential_label.pack(padx=10, pady=8)
 
     def hide(self):
         self.__loginframe.grid_forget()
 
     def show(self):
         self.__loginframe.grid(row=0, column=0)
+    
+    def log(self):
+        self.__mw.log([self.__username_entry.get(), self.__password_entry.get()])
 
-    def return_credentials(self):
-        return [self.__username_entry.get(), self.__password_entry.get()]
+    def move_to_reg(self):
+        self.__loginframe.grid_forget()
+        self.__mw.move_to_reg()
 
 
 class RegistrationWindow(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, mainwindow, parent):
         super().__init__(parent)
+        self.__mw = mainwindow
         self.__registrationframe = ctk.CTkFrame(master=parent)
         self.__registrationframe.grid(row=0, column=0)
 
@@ -65,6 +80,12 @@ class RegistrationWindow(ctk.CTkFrame):
         self.__password_entry = ctk.CTkEntry(master=self.__registrationframe, placeholder_text="Choose a password", show="*")
         self.__password_entry.pack(padx=10, pady=8)
 
+        credential_button = ctk.CTkButton(master=self.__registrationframe, text="Register", command=self.reg)
+        credential_button.pack(padx=10, pady=8)
+
+        credential_label = ctk.CTkButton(master=self.__registrationframe, text="Already have an account?", command=self.move_to_log)
+        credential_label.pack(padx=10, pady=8)
+
         self.hide()
     
     def hide(self):
@@ -73,52 +94,47 @@ class RegistrationWindow(ctk.CTkFrame):
     def show(self):
         self.__registrationframe.grid(row=0, column=0)
 
-    def return_credentials(self):
-        return [self.__username_entry.get(), self.__password_entry.get()]
+    def reg(self):
+        self.__mw.reg([self.__username_entry.get(), self.__password_entry.get()])
+
+    def move_to_log(self):
+        self.__mw.move_to_log()
 
 
 class Calc:
-    def __init__(self, communicator):
+    def __init__(self, cm):
         self.__window = ctk.CTk()
-        self.__window.resizable(width=0,height=0)
-        self.__cm = communicator
+        self.__window.geometry("400x300")
+        self.__cm = cm
 
-        self.__frames = {"login": LoginWindow(self.__window), "registration": RegistrationWindow(self.__window), "calculator": RegistrationWindow(self.__window)}
+        self.__frames = {"login": LoginWindow(self, self.__window), "registration": RegistrationWindow(self, self.__window), "calculator": CalculatorWindow(self, self.__window)}
         self.__current_frame = self.__frames["login"]
-
-        credential_button = ctk.CTkButton(master=self.__window, text="Login", command=self.log_or_reg)
-        credential_button.grid(row=1, column=0, pady=5)
-
-        credential_label = ctk.CTkLabel(master=self.__window, text="Create an account?", cursor="hand2")
-        credential_label.bind("<Button-1>", lambda e: self.move_to_log_or_reg())
-        credential_label.grid(row=4, column=0, pady=5)
 
         self.__frames["login"].show()
         self.__window.mainloop()
 
-    def move_to_log_or_reg(self):
-        if isinstance(self.__current_frame, LoginWindow):
-            self.__current_frame.hide()
-            self.__frames["registration"].show()
-            self.__current_frame = self.__frames["registration"]
-        else:
-            self.__current_frame.hide()
-            self.__frames["login"].show()
-            self.__current_frame = self.__frames["login"]
+    def move_to_log(self):
+        self.__current_frame.hide()
+        self.__frames["login"].show()
+        self.__current_frame = self.__frames["login"]
 
-    def log_or_reg(self):
-        credentials = self.__current_frame.return_credentials()
+    def move_to_reg(self):
+        self.__current_frame.hide()
+        self.__frames["registration"].show()
+        self.__current_frame = self.__frames["registration"]
 
-        if isinstance(self.__current_frame, LoginWindow):
-            if cm.on_login(credentials):
-                self.__current_frame.hide()
-                self.__frames["calculator"].show()
-                self.__current_frame = self.__frames["calculator"]
-        else:
-            if cm.on_register(credentials):
-                self.__current_frame.hide()
-                self.__frames["login"].show()
-                self.__current_frame = self.__frames["login"]
+    def move_to_calc(self):
+        self.__current_frame.hide()
+        self.__frames["calculator"].show()
+        self.__current_frame = self.__frames["calculator"]
+
+    def log(self, credentials):
+        if cm.on_login(credentials):
+            self.move_to_calc()
+
+    def reg(self, credentials):
+        if cm.on_register(credentials):
+            self.move_to_log()
 
 
 if __name__ == "__main__":
