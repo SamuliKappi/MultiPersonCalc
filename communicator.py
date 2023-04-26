67f5926476cf04d42a49ff43344021eb24e2c0c0
Samuli Kappi <samuli.kappi@tuni.fi>
2023-04-26 21:59:57 +0300

    Fixed couple of bugs and added minimum name and password length of 1

--- communicator.py
+++ communicator.py
@@ -6,7 +6,10 @@ class Communicator:
 
     def on_login(self, credentials):
         form = {"name": credentials[0], "password": credentials[1]}
-        response = requests.post("https://127.0.0.1:5000/login", form, verify=False)
+        try:
+          response = requests.post("https://127.0.0.1:5000/login", form, verify=False)
+        except requests.exceptions.ConnectionError:
+          response.status_code = "Connection refused"
         if(response.status_code != 202):
             #HANDLE INVALID CREDENTIALS :)
             return None
@@ -15,18 +18,24 @@ class Communicator:
 
     def on_register(self, credentials):
         form = {"name": credentials[0], "password": credentials[1]}
-        response = requests.post("https://127.0.0.1:5000/login", form)
+        response = requests.post("https://127.0.0.1:5000/signup", form, verify=False)
         print(response.json())
         return response.json()
 
-    def on_post(self, symbol):
+    def on_post(self, symbol, token):
         form = {"input": symbol}
-        response = requests.post("https://127.0.0.1:5000/login", form)
+        response = requests.post("https://127.0.0.1:5000/addNumber", form, verify=False)
         print(response.json())
         return response.json()
 
+    def on_reset(self, token):
+        form = {"token": token}
+        response = requests.get("https://127.0.0.1:5000/reset", form, verify=False)
+        print(response.json())
+        return response.json()
+    
     def get_status(self, token):
         form = {"token": token}
-        response = requests.get("https://127.0.0.1:5000/status", form)
+        response = requests.get("https://127.0.0.1:5000/status", form, verify=False)
         print(response.json())
         return response.json()
