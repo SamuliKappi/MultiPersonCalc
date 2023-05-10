718d9b2920e620fa547d0d244393689e849e52b6
Samuli Kappi <samuli.kappi@tuni.fi>
2023-05-10 17:30:25 +0300

    added error handling when server isnt available

--- communicator.py
+++ communicator.py
@@ -9,42 +9,57 @@ class Communicator:
         try:
             response = requests.post("https://127.0.0.1:5000/login", form, verify=False)
         except requests.exceptions.ConnectionError:
-            response.status_code = "Connection refused"
+            return {"error": "Connection refused"}
         if(response.status_code != 202):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
 
     def on_register(self, credentials):
         form = {"name": credentials[0], "password": credentials[1]}
-        response = requests.post("https://127.0.0.1:5000/signup", form, verify=False)
+        try:
+          response = requests.post("https://127.0.0.1:5000/signup", form, verify=False)
+        except requests.exceptions.ConnectionError:
+            return {"error": "Connection refused"}
         if(response.status_code != 201):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
 
     def on_post(self, symbol, token):
         form = {"input": symbol, "token":token}
-        response = requests.post("https://127.0.0.1:5000/addNumber", form, verify=False)
+        try:
+          response = requests.post("https://127.0.0.1:5000/addNumber", form, verify=False)
+        except requests.exceptions.ConnectionError:
+            return {"error": "Connection refused"}
         if(response.status_code != 200):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
 
     def on_reset(self, token):
         form = {"token": token}
-        response = requests.post("https://127.0.0.1:5000/reset", form, verify=False)
+        try:
+          response = requests.post("https://127.0.0.1:5000/reset", form, verify=False)
+        except requests.exceptions.ConnectionError:
+          return {"error": "Connection refused"}
         if(response.status_code != 200):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
     
     def get_status(self, token):
         form = {"token": token}
-        response = requests.get("https://127.0.0.1:5000/status", form, verify=False)
+        try:
+          response = requests.get("https://127.0.0.1:5000/status", form, verify=False)
+        except requests.exceptions.ConnectionError:
+            return {"error": "Connection refused"}
         if(response.status_code != 200):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
     
     def equals(self, token):
         form = {"token": token}
-        response = requests.post("https://127.0.0.1:5000/equals", form, verify=False)
+        try:
+          response = requests.post("https://127.0.0.1:5000/equals", form, verify=False)
+        except requests.exceptions.ConnectionError:
+            return {"error": "Connection refused"}
         if(response.status_code != 200):
-            return {"message": response.json()["message"]}
+            return {"error": response.json()["message"]}
         return response.json()
